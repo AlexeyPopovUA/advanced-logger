339a35afcd1e3a105a1639b9e309e42fc1e8c9da
apopov <Alexey.Popov@albumprinter.com>
2018-02-09 10:59:42 +0100

    Request sending fix

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,40 +1,39 @@
-import * as http from "http";
+import * as https from "https";
+import * as url from "url";
 
 function nodePostRequest(
     serviceConfig: {
         sourceCategory: string,
         host: string,
         url: string,
-        sourceName?: string
+        sourceName: string
     },
     payload: string): Promise<any> {
 
-    // todo Set url properties from config
+    const urlConfig = url.parse(serviceConfig.url);
+
     const postOptions = {
-        body: payload,
         method: "POST",
-        hostname: "hostname",
-        port: 1234,
-        path: "/pathname",
+        hostname: urlConfig.hostname,
+        port: urlConfig.port,
+        path: urlConfig.pathname
     };
 
     return new Promise((resolve, reject) => {
-        const req = http.request(postOptions, (res) => {
-            console.log(`STATUS: ${res.statusCode}`);
-            console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-            res.setEncoding("utf8");
-            res.on("end", () => {
-                console.log("result");
-                resolve("result!");
-            });
+        const req = https.request(postOptions, response => {
+            console.log(`STATUS: ${response.statusCode}`);
+            console.warn(`response:`, response);
+            resolve({});
         });
 
+        req.setDefaultEncoding("utf8");
         req.setHeader("Content-Type", "application/json");
         req.setHeader("X-Sumo-Category", serviceConfig.sourceCategory);
         req.setHeader("X-Sumo-Host", serviceConfig.host);
-        req.setHeader("X-Sumo-Name", serviceConfig.sourceName || "");
+        req.setHeader("X-Sumo-Name", serviceConfig.sourceName);
 
-        req.on("error", e => reject(e));
+        req.on("error", error => reject(error));
+        req.write(payload);
         req.end();
     });
 }
