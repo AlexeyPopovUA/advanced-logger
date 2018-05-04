c0d216d7afe80b1a45aedbbeb0dfb610d55e6b64
Oleksii Popov <apopov@albelli.net>
2018-05-04 13:34:59 +0200

    Ability to build production versions for browser and nodejs with source maps

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,121 +1,51 @@
-var nodeExternals = require('webpack-node-externals');
-var webpack = require('webpack');
-var browserify = require('browserify');
-var path = require('path');
-var fs = require('fs');
-var PACKAGE_FILE = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json'), 'utf-8'));
-var LIB_NAME = PACKAGE_FILE.name;
+module.exports = env => {
+    console.log(env);
 
-/* helper function to get into build directory */
-var libPath = function(name) {
-    if ( undefined === name ) {
-        return 'dist';
-    }
+    const mode = env.prod === 1 ? 'production' : 'development';
+    const path = require('path');
+    const exportLibraryName = "universalLogger";
+    const browserProd = {
+        mode,
+        outputFile: 'universal-logger.browser.min.js',
+        outputFolder: 'browser',
+        target: 'web',
+        libraryTarget: 'window'
+    };
 
-    return path.join('dist', name);
-}
-
-/* helper to clean leftovers */
-var outputCleanup = function(dir) {
-    if (fs.existsSync(libPath()) === false) {
-        return;
-    }
-
-    var list = fs.readdirSync(dir);
-    for(var i = 0; i < list.length; i++) {
-        var filename = path.join(dir, list[i]);
-        var stat = fs.statSync(filename);
-
-        if(filename === '.' || filename === '..') {
-            // pass these files
-        } else if(stat.isDirectory()) {
-            // outputCleanup recursively
-            outputCleanup(filename, false);
-        } else {
-            // rm fiilename
-            fs.unlinkSync(filename);
-        }
-    }
-    fs.rmdirSync(dir);
-};
-
-/* precentage handler is used to hook build start and ending */
-var percentage_handler = function handler(percentage, msg) {
-    if ( 0 === percentage ) {
-        /* Build Started */
-        outputCleanup(libPath());
-        console.log('Build started... Good luck!');
-    } else if ( 1.0 === percentage ) {
-        // TODO: No Error detection. :(
-        create_browser_version(webpack_opts.output.filename);
-    }
-}
-
-var webpack_opts = {
-    entry: './src/index.ts',
-    target: 'node',
-    output: {
-        filename: libPath('index.js'),
+    const nodeProd = {
+        mode,
+        outputFile: 'universal-logger.node.min.js',
+        outputFolder: 'node',
+        target: 'node',
         libraryTarget: 'commonjs2'
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-        modules: [
-            'node_modules',
-            'src',
-        ]
-    },
-    module: {
-        loaders: [
-            {
-                enforce: 'pre',
-                test: /\.ts$/,
-                loader: 'tslint-loader',
-                exclude: /node_modules/,
-            }, {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                exclude: [
-                    /node_modules/
-                ],
-            },
-        ],
-    },
-    externals: [nodeExternals()],
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.LoaderOptionsPlugin({
-            options: {
-                tslint: {
-                    emitErrors: true,
-                    failOnHint: true
-                }
-            }
-        }),
-        new webpack.ProgressPlugin(percentage_handler)
-    ],
-}
+    };
 
-var create_browser_version = function (inputJs) {
-    let outputName = inputJs.replace(/\.[^/.]+$/, '');
-    outputName = `${outputName}.browser.js`;
-    console.log('Creating browser version ...');
+    const config = env.target === 'browser' ? browserProd : nodeProd;
 
-    let b = browserify(inputJs, {
-        standalone: LIB_NAME,
-    });
+    const libPath = (name = browserProd.outputFolder, target = browserProd.target) => path.join(target, name);
 
-    b.transform('uglifyify', {
-        global: true,
-        sourceMap: false
-    });
-
-    b.bundle(function(err, src) {
-        if ( err != null ) {
-            console.error('Browserify error:');
-            console.error(err);
+    return {
+        entry: './src/index.ts',
+        target: config.target,
+        output: {
+            library: exportLibraryName,
+            filename: libPath(config.outputFile, config.outputFolder),
+            libraryTarget: config.libraryTarget
+        },
+        devtool: "sourcemaps",
+        mode: config.mode,
+        resolve: {
+            extensions: ['.ts', '.js'],
+            modules: [
+                'node_modules',
+                'src',
+            ]
+        },
+        module: {
+            rules: [
+                {test: /\.ts$/, loader: "awesome-typescript-loader"},
+                {enforce: "pre", test: /\.js$/, loader: "source-map-loader"}
+            ],
         }
-    }).pipe(fs.createWriteStream(outputName));
-}
-
-module.exports = webpack_opts;
+    };
+};
