f35988247517f8dbc47f72f68750899996098e1c
Oleksii Popov <alexejpopovua@gmail.com>
2018-08-19 19:17:09 +0200

    Integration test for InstantStrategy

---
+++ __tests__/strategy/InstantStrategy.spec.ts
@@ -0,0 +1,46 @@
+"use strict";
+
+import "jest";
+import * as sinon from "sinon";
+import {AdvancedLogger} from "../../src";
+import {service} from "../../src";
+import {strategy} from "../../src";
+
+const SumologicService = service.SumologicService;
+const InstantStrategy = strategy.InstantStrategy;
+
+// todo Rewrite stubs with jest functionality
+const sandBox = sinon.sandbox.create();
+
+describe("InstantStrategy", () => {
+    let logger: AdvancedLogger;
+
+    const config = {
+        defaultLogConfig: {},
+        serviceConfig: {}
+    };
+
+    afterEach(() => {
+        sandBox.restore();
+
+        if (logger) {
+            logger.destroy();
+            logger = null;
+        }
+    });
+
+    it("Should deliver logs instantly to the service", done => {
+        logger = new AdvancedLogger({
+            service: new SumologicService(config),
+            strategy: new InstantStrategy()
+        });
+
+        sandBox.stub(SumologicService.prototype, "sendAllLogs").callsFake(logs => {
+            expect(logs.length).toBe(1);
+            done();
+            return Promise.resolve();
+        });
+
+        logger.log({test: "test123"});
+    });
+});
