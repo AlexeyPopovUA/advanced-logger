d7d28c4f1b84801d9ae0f8dec58fe4a0a3483850
Oleksii Popov <alexejpopovua@gmail.com>
2018-08-22 21:17:04 +0200

    Shared sonar config

--- sonar.sh
+++ sonar.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-exec sonar-scanner \
-  -Dsonar.projectKey=advanced-logger \
-  -Dsonar.projectVersion=1.0.9 \
-  -Dsonar.sourceEncoding=UTF-8 \
-  -Dsonar.organization=alexeypopovua-github \
-  -Dsonar.sources=./src \
-  -Dsonar.tests=./__tests__ \
-  -Dsonar.typescript.lcov.reportPaths=coverage/lcov.info \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=<login token>
+exec sonar-scanner
