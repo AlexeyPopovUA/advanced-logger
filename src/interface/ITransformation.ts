defae684b3b2b6a81f9d006ae48b7707f7e2e6cd
Oleksii Popov <apopov@albelli.net>
2018-09-12 14:52:45 +0200

    todo Improve types

--- src/interface/ITransformation.ts
+++ src/interface/ITransformation.ts
@@ -1,6 +1,7 @@
-import {TransformationEnum} from "../enums/TransformationEnum";
+import {TransformationEnum} from "..";
 
 export default interface ITransformation {
     type: TransformationEnum;
+    //todo Add the config type
     configuration?: any;
 }
