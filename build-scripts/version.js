be240b95ed55f0322c64197c75c7d6dab3a06242
Oleksii Popov <alexejpopovua@gmail.com>
2018-11-03 18:07:04 +0100

    yml-deployment "Replace version" script

---
+++ build-scripts/version.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const currentVersion = require("./../package.json").version;
+const file = "./sonar-project.properties";
+
+fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+        return console.log(err);
+    }
+    const result = data.replace(/sonar\.projectVersion=\d\.\d\.\d/g, `sonar.projectVersion=${currentVersion}`);
+
+    fs.writeFile(file, result, 'utf8', err => {
+        if (err) return console.log(err);
+        console.log(`Version ${currentVersion} was applied to sonar-project.properties`);
+    });
+});
