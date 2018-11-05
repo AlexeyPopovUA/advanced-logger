8cda61e1c66ce07498daf035673035717d8a372b
Oleksii Popov <alexejpopovua@gmail.com>
2018-11-05 15:18:37 +0100

    interfaces Sonar warnings fix

--- src/service/ConsoleService.ts
+++ src/service/ConsoleService.ts
@@ -4,11 +4,11 @@ import IService from "../interface/IService";
  * Console reporting service for debugging purposes
  */
 export default class ConsoleService implements IService {
-    public preparePayload<T>(logs: T[]): Promise<any> {
+    public preparePayload<T>(logs: T[]): Promise<T[]> {
         return Promise.resolve(logs);
     }
 
-    public sendAllLogs<T>(logs: T[]): Promise<any> {
+    public sendAllLogs<T>(logs: T[]): Promise<void> {
         console.log(logs);
         return Promise.resolve();
     }
