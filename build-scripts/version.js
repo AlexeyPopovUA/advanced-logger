3a54b820f99892ed8a1b1a55214daf24289e6799
Oleksii Popov <alexejpopovua@gmail.com>
2018-11-05 15:48:05 +0100

    fix(version) Fix for replacement of version in Sonar config

--- build-scripts/version.js
+++ build-scripts/version.js
@@ -6,7 +6,7 @@ fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
         return console.log(err);
     }
-    const result = data.replace(/sonar\.projectVersion=\d\.\d\.\d/g, `sonar.projectVersion=${currentVersion}`);
+    const result = data.replace(/sonar\.projectVersion=\d+\.\d+\.\d+/g, `sonar.projectVersion=${currentVersion}`);
 
     fs.writeFile(file, result, 'utf8', err => {
         if (err) return console.log(err);
